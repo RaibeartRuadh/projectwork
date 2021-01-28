8b15f8ba884e6a10f5bf347263c6a41a3856c437
RaibeartRuadh <robroyway@gmail.com>
2021-01-28 03:02:44 +0300

    fix df message

--- playbook/roles/tele/files/df.sh
+++ playbook/roles/tele/files/df.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "`df | grep "/dev/sda1" | awk '{print $5}' | sed 's/\%//'`" -ge 10 ]; 
- then echo "Disk usage exceeded 95%" | curl -s -X POST https://api.telegram.org/bot1493001337:AAEHNRl87mm47EfqY-d4b-_RWWcEvrZuANk/sendMessage -d chat_id=-469980725 -d text="Info - диск заполнен на 10%! - проверка";
+ then echo "Disk usage exceeded 95%" | curl -s -X POST https://api.telegram.org/bot1493001337:AAEHNRl87mm47EfqY-d4b-_RWWcEvrZuANk/sendMessage -d chat_id=-469980725 -d text="Info - Тестовое уведомление. Диск заполнен больше чем на 10%!";
 fi
 
 if [ "`df | grep "/dev/sda1" | awk '{print $5}' | sed 's/\%//'`" -ge 95 ]; 
