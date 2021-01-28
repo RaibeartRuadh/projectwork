4276fef38c3330f0fd295367a14a0bbd443aa713
RaibeartRuadh <38784332+RaibeartRuadh@users.noreply.github.com>
2021-01-28 18:41:43 +0300

    Update df.sh

--- playbook/roles/tele/files/df.sh
+++ playbook/roles/tele/files/df.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [ "`df | grep "/dev/sda1" | awk '{print $5}' | sed 's/\%//'`" -ge 10 ]; 
- then echo "Disk usage exceeded 95%" | curl -s -X POST https://api.telegram.org/bot1493001337:AAEHNRl87mm47EfqY-d4b-_RWWcEvrZuANk/sendMessage -d chat_id=-469980725 -d text="Info - Тестовое уведомление. Диск заполнен больше чем на 10%!";
+ then echo "Disk usage exceeded 95%" | curl -s -X POST https://api.telegram.org/bot14!!!!337:AA!!!!!!!7EfqY-d4b-_RWWcEvrZuANk/sendMessage -d chat_id=-4!!!!!!725 -d text="Info - Тестовое уведомление. Диск заполнен больше чем на 10%!";
 fi
 
 if [ "`df | grep "/dev/sda1" | awk '{print $5}' | sed 's/\%//'`" -ge 95 ]; 
- then echo "Disk usage exceeded 95%" | curl -s -X POST https://api.telegram.org/bot1493001337:AAEHNRl87mm47EfqY-d4b-_RWWcEvrZuANk/sendMessage -d chat_id=-469980725 -d text="Warning - диск заполнен на 95%!";
+ then echo "Disk usage exceeded 95%" | curl -s -X POST https://api.telegram.org/bo!!!!!!!!!!337:AA!!!!!!!!!qY-d4b-_RWWcE!!ANk/sendMessage -d chat_id=-4!!!!980725 -d text="Warning - диск заполнен на 95%!";
 fi
 
 if [ "`df | grep "/dev/sda1" | awk '{print $5}' | sed 's/\%//'`" -ge 99 ]; 
 
-then echo "Disk usage exceeded 99%!" | curl -s -X POST https://api.telegram.org/bot1493001337:AAEHNRl87mm47EfqY-d4b-_RWWcEvrZuANk/sendMessage -d chat_id=-469980725 -d text="Critical - диск заполнен на 99%!";
+then echo "Disk usage exceeded 99%!" | curl -s -X POST https://api.telegram.org/b!!!!!!!!!337:AAE!!!!!!!!!!qY-d4b-_RWWc!!!!uANk/sendMessage -d chat_id=-4!!!!!!25 -d text="Critical - диск заполнен на 99%!";
 
 fi
 
